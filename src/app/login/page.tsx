9c3c86b16b5cc19d2e50c454ad4b5e9bede5bdf1
Minh Nguyen <minhhien134@gmail.com>
2024-07-04 16:01:00 +0700

    modify

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,28 +4,29 @@ import { signIn } from "next-auth/react";
 import { useState } from "react";
 import Button from "src/components/Button/Button";
 import styles from "./login.module.scss";
+import Link from "next/link";
 
 export default function LoginPage() {
-  const [errorMessage, setErrorMessage] = useState("")
+  const [errorMessage, setErrorMessage] = useState("");
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const form = event.currentTarget
+    const form = event.currentTarget;
     const formData = new FormData(form);
-    const credentials = Object.fromEntries(formData)
-    console.log("credentials", credentials)
+    const credentials = Object.fromEntries(formData);
+    console.log("credentials", credentials);
 
     const result = await signIn("credentials", {
       redirect: false,
       email: credentials.email,
-      password: credentials.password
-    })
-    console.log("RESULT", result)
+      password: credentials.password,
+    });
+    console.log("RESULT", result);
 
     if (result?.error) {
-      setErrorMessage(result.error)
+      setErrorMessage(result.error);
     } else {
-      window.location.href="/"
+      window.location.href = "/";
     }
   }
 
@@ -34,11 +35,19 @@ export default function LoginPage() {
       <div className="container">
         <form onSubmit={handleSubmit} className={styles["login-form"]}>
           <input type="email" name="email" placeholder="Your email" required />
-          <input type="password" name="password" placeholder="Your password" required />
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            required
+          />
           <Button type="submit">Login</Button>
+          <div>
+            Don't have an account yet? <Link href="/sign-up">Sign up</Link> now
+          </div>
           {errorMessage && <p>{errorMessage}</p>}
         </form>
       </div>
     </main>
-  )
+  );
 }
